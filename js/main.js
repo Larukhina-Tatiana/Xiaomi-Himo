ebcdd77a6c45f90af47106f90b72210feec01a58
Татьяна <ltasya72@gmail.com>
2023-05-28 13:11:57 +0300

    перед адаптивом

--- js/main.js
+++ js/main.js
@@ -7,3 +7,18 @@ $(function () {
     autoplaySpeed: 3000
   });
 })
+
+$(".nav__list, .cover__content").on("click","a", function (event) {
+  //отменяем стандартную обработку нажатия по ссылке
+  event.preventDefault();
+
+  //забираем идентификатор бока с атрибута href
+  var id  = $(this).attr('href'),
+
+  //узнаем высоту от начала страницы до блока на который ссылается якорь
+    top = $(id).offset().top;
+  
+  //анимируем переход на расстояние - top за 1500 мс
+  $('body,html').animate({scrollTop: top}, 1500);
+});
+  
